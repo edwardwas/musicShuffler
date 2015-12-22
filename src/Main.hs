1743bd108e3d9b67b07541f5b1f5c254240e5b55
Edward Wastell <ejw59@.bath.ac.uk>
2015-12-22 12:14:02 +0000

    Home time!

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Turtle
+import qualified Data.Text as T
+import Filesystem.Path.CurrentOS hiding (empty)
+import Control.Applicative
+import Control.Monad.Random
+import Control.Monad
+import Prelude hiding (FilePath)
+
+musicExtensions :: [Text]
+musicExtensions = ["mp3","ogg","m4a","wav","flac","wma"]
+
+addRandomPrep :: MonadIO m => FilePath -> m FilePath
+addRandomPrep fp = do
+    rs <- liftIO $ replicateM 5 (getRandomR ('a','z'))
+    return $ directory fp </> decodeString (rs <> "_" <> encodeString (filename fp))
+        
+musicPattern ::  Pattern Text
+musicPattern = choice $ 
+    map (suffix . text . mappend "." ) musicExtensions
+
+isRandomPref :: Pattern Text
+isRandomPref = mappend musicPattern $ T.pack <$> prefix ((count 5 lower) <> (pure <$> char '_')) 
+
 main = putStrLn "Not doen yet"
